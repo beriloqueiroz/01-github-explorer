638dbfee98d0bd83c5fa73ce9ad0a9bf46310e7e
berilo queiroz <beriloqueiroz@gmail.com>
2021-11-25 16:43:17 -0300

    first commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.scss'
+import { useState, useEffect } from "react"
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+    useEffect(()=> {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data=>setRepositories(data))
+    },[])
+    console.log(repositories)
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                {repositories.map(repo=><RepositoryItem key={repo.name} repository={repo} />)}
+            </ul>
+        </section>
+    )
+}
